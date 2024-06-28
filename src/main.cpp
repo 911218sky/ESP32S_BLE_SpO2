906d4e28f190cb7c8e185a553dcd9c3bd5c80836
Chiu Chun-Hsien <3414061123aa@gmail.com>
2024-06-28 12:44:02 +0800

    Changes to be committed: 	modified:   platformio.ini 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@ bool deviceConnected = false;
 float heartRate = -1;
 float spO2 = -1;
 
+int __round(float value)
+{
+  return static_cast<int>(value >= 0.0f ? value + 0.5f : value - 0.5f);
+}
+
 void BLEHandlerCharacteristicCallbacks::onRead(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param)
 {
   if (DEBUG_LEVEL >= 1)
@@ -18,9 +23,13 @@ void BLEHandlerCharacteristicCallbacks::onRead(BLECharacteristic *pCharacteristi
   // Create a buffer to store the data
   uint8_t data[8];
 
+  // Round the heart rate and spO2 values
+  int roundedHeartRate = __round(heartRate);
+  int roundedSpO2 = __round(spO2);
+
   // Copy the heartRate and spO2 data into the data array using memcpy
-  memcpy(data, &heartRate, sizeof(heartRate));           // Copy heartRate
-  memcpy(data + sizeof(heartRate), &spO2, sizeof(spO2)); // Copy spO2
+  memcpy(data, &roundedHeartRate, sizeof(roundedHeartRate));             // Copy heartRate
+  memcpy(data + sizeof(roundedSpO2), &roundedSpO2, sizeof(roundedSpO2)); // Copy spO2
 
   // Set the data to the characteristic and send it to the client
   pCharacteristic->setValue(data, sizeof(data));
